93f355e17c47fdc6697af585ce2290ae633144ea
BikashShiwakoti <168492490+BikashShiwakoti@users.noreply.github.com>
2024-05-13 09:53:45 -0400

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,69 +1,39 @@
-import logo from '../Images/logoo.jpg'
-import './Header.css'
+import profile from '../Images/bikash.JPG'
+import './Home.css'
+function Home(){
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = '../BikashShiwakoti.docx'; // Adjust the path as needed
+        link.setAttribute('download', 'BikashShiwakoti.docx');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
 
-function Header() {
-    return (
-        <div >
+    return(
+        <div id='home' className="aboutme ">
+            <div className='grid grid-rows-2 items-center justify-center'>
+                <div className='flex items-center justify-center'> 
+                <img  src={profile} alt='Bikash' className='profile '/>
+
+                </div>
+                <div>
+                <h1><span>Hi, it's me Bikash Shiwakoti.</span> I am a software developer based on Canada.</h1>
+            <p>I recently graduate from Mohak colleege from Software course securing Dean's Honour list.<br/><br/>
+                I am eager and really passionate about to explore more in programming world.
+            </p>
+                </div>
+            </div>
            
+            <div className='myself'>
+            <a href="https://www.linkedin.com/in/bikashshiwakoti" className="connect">Connect</a>
 
-
-<nav className="bg-gray-400">
-  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div className="relative flex h-16 items-center justify-between">
-      <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="absolute -inset-0.5"></span>
-          <span className="sr-only">Open main menu</span>
-         
-          <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-         
-          <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center">
-          <img className="h-8 w-auto"   src={logo} alt="logo" />
+            <div>
+      <button onClick={handleDownload}>Download Resume</button>
+    </div>            </div>
         </div>
-        <div className="hidden sm:ml-6 sm:block">
-          <div className="flex space-x-4 gap-4">
-            <a href="#home" className=" bg-white  rounded-md px-3 py-2 text-sm font-medium anchor-link" aria-current="page">Home</a>
-            <a href="#about" className=" bg-white  hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium anchor-link">About Me</a>
-            <a href="#project" className=" bg-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium anchor-link">Projects</a>
-            <a href="#contact" className=" bg-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium anchor-link">Contact</a>
-          </div>
-        </div>
-      </div>
-      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
         
-      <a href="https://www.linkedin.com/in/bikashshiwakoti" className="nav-connect">Connect</a>
-
-       
-        
-      </div>
-    </div>
-  </div>
-
-  <div className="sm:hidden" id="mobile-menu">
-    <div className="space-y-1 px-2 pb-3 pt-2">
-      <a href="#home" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-      <a href="#about" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About Me</a>
-      <a href="#project" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-      <a href="#contact" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
-    </div>
-  </div>
-</nav>
-
-
-
-        </div>
-
-
-
     )
 }
 
-export default Header;
+export default Home;
