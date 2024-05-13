84f8533f7c119e81cc94745021e4d71e47f63433
BikashShiwakoti <168492490+BikashShiwakoti@users.noreply.github.com>
2024-05-13 19:28:30 -0400

    Update Header.js
    Formatted well

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,6 @@ import './Header.css'
 function Header() {
     return (
         <div >
-           
-
-
 <nav className="bg-gray-400">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="relative flex h-16 items-center justify-between">
@@ -39,10 +36,7 @@ function Header() {
       </div>
       <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
         
-      <a href="https://www.linkedin.com/in/bikashshiwakoti" className="nav-connect">Connect</a>
-
-       
-        
+      <a href="https://www.linkedin.com/in/bikashshiwakoti" className="nav-connect">Connect</a>           
       </div>
     </div>
   </div>
