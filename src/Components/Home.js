cb594d0deb937dfb4bf89be36aa67bb2ebe7c2b7
bikashshiwakoti <bikashsiwakoti486@gmail.com>
2024-05-13 20:06:14 -0400

    Made resume downloadable

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,38 +1,50 @@
 import profile from '../Images/bikash.JPG'
+import resume from '../Assets/Resume.pdf'
 import './Home.css'
-function Home(){
-    const handleDownload = () => {
-        const link = document.createElement('a');
-        link.href = '../BikashShiwakoti.docx'; // Adjust the path as needed
-        link.setAttribute('download', 'BikashShiwakoti.docx');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      };
+function Home() {
 
-    return(
+
+    return (
         <div id='home' className="aboutme ">
             <div className='grid grid-rows-2 items-center justify-center'>
-                <div className='flex items-center justify-center'> 
-                <img  src={profile} alt='Bikash' className='profile '/>
+                <div className='flex items-center justify-center'>
+                    <img src={profile} alt='Bikash' className='profile ' />
 
                 </div>
                 <div>
-                <h1><span>Hi, it's me Bikash Shiwakoti.</span> I am a software developer based on Canada.</h1>
-            <p>I recently graduate from Mohak colleege from Software course securing Dean's Honour list.<br/><br/>
-                I am eager and really passionate about to explore more in programming world.
-            </p>
+                    <h1><span>Hi, it's me Bikash Shiwakoti.</span> I am a software developer based on Canada.</h1>
+                    <p>I recently graduate from Mohak colleege from Software course securing Dean's Honour list.<br /><br />
+                        I am eager and really passionate about to explore more in programming world.
+                    </p>
                 </div>
             </div>
-           
-            <div className='myself'>
-            <a href="https://www.linkedin.com/in/bikashshiwakoti" className="connect">Connect</a>
 
-            <div>
-      <button onClick={handleDownload}>Download Resume</button>
-    </div>            </div>
+            <div className='myself'>
+                
+                    <div>
+                <button type="button" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mr-10">
+                        <a
+                            className="button"
+                            href="https://www.linkedin.com/in/bikashshiwakoti"
+                        >
+                           Connect
+                        </a>
+                    </button>
+                    <button type="button" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        <a
+                            className="button"
+                            href={resume}
+                            download
+                        >
+                            Download Resume
+                        </a>
+                    </button>
+
+
+                </div>
+            </div>
         </div>
-        
+
     )
 }
 
